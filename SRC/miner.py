b5fcc7eadfb62ca42886f015fa13459e2fa5ff67
CanonU <142544235+CanonU@users.noreply.github.com>
2024-02-29 10:28:42 -0700

    Update miner.py

--- SRC/miner.py
+++ SRC/miner.py
@@ -1,1 +1,12 @@
+import pygame
+class Miner:
+    def __init__(self,name,cost,power,buy):
+        self.cost = cost
+        self.power = power
+        self.buy = buy
+    def __str__(self):
+     return f"Miner(name ='{self.name}', cost={self.cost}, power={self.power}), buy={self.buy}"
+      
 
+  
+      
