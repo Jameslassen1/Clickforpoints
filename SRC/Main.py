95d868fc26127a712da4bd432d79ccea83144ef2
Jameslassen1 <142922172+Jameslassen1@users.noreply.github.com>
2024-03-01 09:54:44 -0700

    the buton

--- SRC/Main.py
+++ SRC/Main.py
@@ -1,23 +1,25 @@
 import pygame
 from Button import Button
 
-
+score = 0
 
 def function1():
-  print("Button 1 clicked")
+  global score
+  score += 1
+  print(score)
 
 
-#def function2():
-  #print("Button 2 clicked")
+def function2():
+  print("Button 2 clicked")
 
 pygame.init()
-screen = pygame.display.set_mode((400, 200))
-#clock = pygame.time.Clock()
+screen = pygame.display.set_mode((600, 600))
+clock = pygame.time.Clock()
 
 font = pygame.font.Font(None, 36)
 
-button1 = Button(50, 50, 150, 50, (0, 255, 0), "Button 1", font, function1)
-#button2 = Button(200, 50, 150, 50, (0, 0, 255), "Button 2", font, function2)
+button1 = Button(200, 200, 150, 50, (150,150,150), "rock", font, function1)
+button2 = Button(400, 50, 150, 50, (0, 0, 255), "miner 1", font, function2)
 
 running = True
 while running:
@@ -26,13 +28,13 @@ while running:
         if event.type == pygame.QUIT:
             running = False
         button1.handle_event(event)
-        #button2.handle_event(event)
+        button2.handle_event(event)
 
     button1.draw(screen)
-    #button2.draw(screen)
+    button2.draw(screen)
 
     pygame.display.flip()
-    #clock.tick(60)
+    clock.tick(60)
     pygame.display.update()
 
 
