a55c16ea4df1bce0f1e70434b49cf10706515a6d
CanonU <142544235+CanonU@users.noreply.github.com>
2024-03-01 10:54:29 -0700

    Update miner.py

--- SRC/miner.py
+++ SRC/miner.py
@@ -1,11 +1,13 @@
 import pygame
 class Miner:
-    def __init__(self,name,cost,power):
+    #Miner cost is (cost= b(1.25)^x where b =base cost and x = owned miners 
+    def __init__(self,name,cost,power,amount):
         self.name = name
         self.cost = cost
         self.power = power
+        self,amount = amount
     def __str__(self):
-     return f"Miner(name ='{self.name}', cost={self.cost}, power={self.power})"
+     return f"Miner(name ='{self.name}', cost={self.cost}, power={self.power} amount={self.amount})"
       
 
   
