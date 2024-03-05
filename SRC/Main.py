b34ef777536685badf23e3c3f22cfb7e8fe44654
CanonU <142544235+CanonU@users.noreply.github.com>
2024-03-05 10:07:30 -0700

    start screen

--- SRC/Main.py
+++ SRC/Main.py
@@ -7,7 +7,7 @@ from ImageButton import ImageButton
 Miner.power =0
 score = 0
 Miner1 = Miner("Miner",10,0,0)
-
+play = False
 def function1():
   global score
   score += 1
@@ -21,23 +21,30 @@ def function2():
   print("Miner1 Amount:", Miner1.amount)
   print("Button 2 clicked")
 
+
 pygame.init()
 screen = pygame.display.set_mode((600, 600))
 clock = pygame.time.Clock()
 font = pygame.font.Font(None, 36)
-
+while play == False:
+  start_text = font.render("Welcome to Pebbles.", True, (0,0,0))
+  screen.blit(start_text, (300 + 39, 300 + 9))
+  pygame.display.update()
+  for event in pygame.event.get():
+    if event.type == pygame.MOUSEBUTTONDOWN:
+      play = True
 button1_image = pygame.image.load("images/rock.png")
 MinerImage = pygame.image.load("images/EthanViking_Miner.png")
 
 button1 = ImageButton(300,300, button1_image, function1)
 button2 = Button(400,20, 150, 50, (0, 0, 255), "miner 1", font, function2, MinerImage)
 
-running = True
-while running:
+
+while play:
     screen.fill((255, 255, 255))
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            running = False
+            play = False
         button1.handle_event(event)
         button2.handle_event(event)
 
